80058bf3d2dea092ea6eade47dfb9c6d371b56d4
Louis Bovay <54527880+EctoBoggan@users.noreply.github.com>
2020-10-14 03:00:49 +0200

    Update GestionActiveDirectory.ps1
    Ajout de commentaires

--- GestionActiveDirectory/GestionActiveDirectory.ps1
+++ GestionActiveDirectory/GestionActiveDirectory.ps1
@@ -1,10 +1,12 @@
-﻿while($true){
+# L'utilisateur peut faire le nombre d'action qu'il souhaite tant qu'il ne quite pas l'application
+while($true){
+
     # Demande à l'utilisateur de faire un choix
     "1. Créez les OU"
     "2. Ajouter des utilisateurs"
     "3. Supprimer les utilisateurs"
     "4. Supprimer les OU"
-    "5. Quitter"
+    "5. Quitter l'application"
     $choix = Read-Host -Prompt "Saisissez votre choix "
 
     # Vérification de la réponse de l'utilisateur
@@ -21,13 +23,17 @@
     $currentPath = $PSScriptRoot
 
     #En fonction de son choix, ouvre un fichier (Invoke-Expression permet d'executer un commande String)
-
     switch ($choix)
     {
+        # Créer les OU
         1 {Invoke-Expression "$currentPath\scripts\newOU.ps1"}
+        # Ajouter des utilisateurs
         2 {Invoke-Expression "$currentPath\scripts\AddUser.ps1"}
+        # Supprimer tout les utilisateurs
         3 {Invoke-Expression "$currentPath\scripts\removeUser.ps1"}
+        # Supprimer toutes les OU
         4 {Invoke-Expression "$currentPath\scripts\removeOU.ps1"}
+        # Quitter l'application
         5 {exit}
     }
 }
