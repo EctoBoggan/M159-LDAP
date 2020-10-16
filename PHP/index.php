ff0437cdb862d8c87295f9ef151955c6d2062c7d
Louis Bovay <54527880+EctoBoggan@users.noreply.github.com>
2020-10-16 04:39:28 +0200

    Update index.php
    Ajout de commentaire, de lignes d'infos et de balise HTML

--- PHP/index.php
+++ PHP/index.php
@@ -1,15 +1,23 @@
+<!DOCTYPE html>
+<link rel='stylesheet' type='text/css' href='style.css' />
+<header>MODULE 159</header>
 <!--Formulaire-->
 <form action="" method="post">
-    <p>Choisisez entre importer les données depuis Active directory vers la Base de données ou de supprimer les données de la base de données</p>
-    <p><input type="submit" value="Importer les données" name="btn"></p>
-    <p><input type="submit" value="Supprimer les données" name="btn"></p>
+    <div class="container">
+        <div class="center">
+            <p class="title">Choisisez entre importer les données depuis Active directory vers la Base de données ou de supprimer les données de la base de données</p>
+            <p><input type="submit" class="shiny-btn" value="Importer les données" name="btn"></p>
+            <p><input type="submit" class="shiny-btn" value="Supprimer les données" name="btn"></p>
+        </div>
+    </div>
 </form>
 
-
+<!--CODE PHP-->
 <?php
 // Détection de la pression d'un bouton par l'utilisateur
 if (isset($_REQUEST['btn'])) {
 
+    echo "<h2> Console :</h2>";
     // Traitement du choix du bouton
     switch ($_REQUEST['btn']) {
             // Bouton import
@@ -28,49 +36,90 @@ if (isset($_REQUEST['btn'])) {
             //Parfois choisir la version du protocol
             //ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-            echo "Connexion à active directory .  .  .<br>";
+            //info
+            echo "<p class='info'>Connexion à active directory .  .  .<br></p>";
 
             //Connection
             if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-                echo "Connection réussie, importation des utilisateurs .  .  .<br>";
+                //info
+                echo "<p class='success'> → Connection réussie</p>";
+                echo "<p class='info'>importation des utilisateurs .  .  .<br></p>";
 
+                //récupération des utilisateur
                 $entries = getUsers($ldap_con);
-                echo (count($entries) -1) , " utilisateurs importés<br>";
+
+                //info
+                echo "<p class='success'> → " . (count($entries) - 1) . " utilisateurs importés<br></p>";
 
                 //Afficher tout les utilisateurs
                 //displayList($entries);
 
+                //Ouverture de la connexion
                 $connexion = dbConnect();
 
                 //Création et insertion de tout les utilisateurs (l'entrée 0 est vide, donc on l'évite)
+                //info
+                echo "<p class='info'>insertion des nouveaux utilisateurs .  .  .<br></p>";
                 $count = 0;
                 foreach ($entries as $user) {
                     if ($count != 0) {
                         $SQLuser = generateUser($user);
-                        
-                        $query = "INSERT INTO tb_user VALUES(null," . $SQLuser['role'] . ",'" . $SQLuser['nom']  . "','" . $SQLuser['prenom']
-                        . "','" . $SQLuser['username'] . "','" . $SQLuser['mail'] . "'," . $SQLuser['tel'] . ");";
 
-                        if($connexion->query($query)===TRUE){
-                            //ça continue
-                        }else{
-                            echo "Erreur : " . $query . "<br>" . $connexion->error;
+                        $query = "INSERT INTO tb_user 
+                        VALUES(null," .
+                            $SQLuser['role'] . ",'" .
+                            $SQLuser['nom']  . "','" .
+                            $SQLuser['prenom'] . "','" .
+                            $SQLuser['username'] . "','" .
+                            $SQLuser['mail'] . "'," .
+                            $SQLuser['tel'] . ");";
+                        //Exécution et vérification de la requête
+                        if ($connexion->query($query) === TRUE) {
+                            //rien à signaler
+                        } else {
+                            //Erreur
+                            echo "<p class='fail'>Erreur : " . $query . "<br>" . $connexion->error . "</p>";
                         }
                     }
                     $count++;
                 }
 
+                //info
+                echo "<p class='success'> → " . ($count - 1) . " nouveaux utilisateurs</p>";
+
+                //Fermeture de la connexion
                 $connexion->close();
-                
             } else {
-                echo "Echec de connection à l'ective directory<br>";
+                //info
+                echo "<p class='fail'>Echec de connection à l'ective directory<br></p>";
             }
             break;
             // Bouton de suppression
         case "Supprimer les données":
-            echo "Suppression des données . . .<br>";
 
-            echo "Données supprimées<br>";
+            //Ouverture de la connexion
+            $connexion = dbConnect();
+
+            //info
+            echo "<p class='info'>Suppression des entrées dans tb_user . . .<br></p>";
+
+            //Compte des entrées
+            $query = "SELECT count(*) as total from tb_user;";
+            //Exécution de la requête
+            $result = $connexion->query($query);
+            //Réception des données de la requête
+            $data = mysqli_fetch_assoc($result);
+
+            //Suppression de donnée
+            $query = "DELETE FROM tb_user;";
+            //Exécution de la requête
+            $connexion->query($query);
+
+            //info
+            echo "<p class='success'> → " . $data['total'] . " entrées supprimées<br></p>";
+
+            //Fermeture de la connexion
+            $connexion->close();
             break;
     }
 }
@@ -101,7 +150,7 @@ function getUsers($connexion)
     //filtre (* pour tout les utilisateurs)
     $filter = "(cn=*)";
     //Recherche
-    $result = ldap_search($connexion, "ou=M159,dc=M159LDAP,dc=LOCAL", $filter) or exit("Recherche échouée");
+    $result = ldap_search($connexion, "ou=M159,dc=M159LDAP,dc=LOCAL", $filter) or exit("<p class='fail'>Recherche échouée</p>");
     //Récupération du résultat
     return ldap_get_entries($connexion, $result);
 }
@@ -128,7 +177,7 @@ function generateUser($user)
         "role" => 0,
         "username" => $user['userprincipalname'][0],
         "mail" => $user['mail'][0],
-        "tel" => intval($user['telephonenumber'][0])
+        "tel" => $user['telephonenumber'][0]
     );
 
     // Gestion clé étrangère
@@ -153,19 +202,26 @@ function generateUser($user)
     return $myUser;
 }
 
-function dbConnect(){
+/**
+ * Ouvre une connexion à la base de donnée
+ * 
+ * @return db la connexion
+ */
+function dbConnect()
+{
 
-    echo "Connexion à la base de donnée .  .  .<br>";
+    echo "<p class='info'>Connexion à la base de donnée .  .  .<br></p>";
     $user = 'root';
     $pass = '';
     $dbname = 'm159';
 
-    $db = new mysqli('localhost',$user,$pass,$dbname) or die("echec de connexion");
-    echo "connexion à la base de donnée réuissie<br>";
+    $db = new mysqli('localhost', $user, $pass, $dbname) or die("<p class='fail'>echec de connexion</p>");
+    echo "<p class='success'> → connexion à la base de donnée réuissie<br></p>";
 
     return $db;
 }
 ?>
 
-
-<p>&copy; Louis Bovay</p>
+<footer>
+    <p>&copy; Louis Bovay</p>
+</footer>
