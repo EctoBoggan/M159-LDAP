9e45cfcd3925c4d3e0ad45da9767a6031b6ad693
Louis Bovay <54527880+EctoBoggan@users.noreply.github.com>
2020-10-17 18:21:26 -0700

    Update GestionActiveDirectory.ps1

--- GestionActiveDirectory/GestionActiveDirectory.ps1
+++ GestionActiveDirectory/GestionActiveDirectory.ps1
@@ -1,15 +1,17 @@
 # L'utilisateur peut faire le nombre d'action qu'il souhaite tant qu'il ne quite pas l'application
 while($true){
 
-    # Demande à l'utilisateur de faire un choix
-    "1. Créez les OU"
+    # Demande a l'utilisateur de faire un choix
+    "� Louis Bovay"
+    ""
+    "1. Creer les OU"
     "2. Ajouter des utilisateurs"
     "3. Supprimer les utilisateurs"
     "4. Supprimer les OU"
     "5. Quitter l'application"
     $choix = Read-Host -Prompt "Saisissez votre choix "
 
-    # Vérification de la réponse de l'utilisateur
+    # Verification de la reponse de l'utilisateur
     while($choix -ne 1 -AND $choix -ne 2 -AND $choix -ne 3 -AND $choix -ne 4 -AND $choix -ne "5")
     {    
         $choix = Read-Host "choix incorrect, réessayez"
@@ -19,13 +21,13 @@ while($true){
     cls
     "`r`n"
 
-    # Récupération du chemin actuel
+    # Recuperation du chemin actuel
     $currentPath = $PSScriptRoot
 
     #En fonction de son choix, ouvre un fichier (Invoke-Expression permet d'executer un commande String)
     switch ($choix)
     {
-        # Créer les OU
+        # Creer les OU
         1 {Invoke-Expression "$currentPath\scripts\newOU.ps1"}
         # Ajouter des utilisateurs
         2 {Invoke-Expression "$currentPath\scripts\AddUser.ps1"}
